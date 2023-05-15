94d47b0ce9f423f727ec790786951644342a5006
coolwebsite187 <133351759+coolwebsite187@users.noreply.github.com>
2023-05-15 10:46:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def change_innerhtmlandtext(id,html,text):
   change_innerhtml(id,html)
   change_text(id,text)
 
-def adolf(id,targetid,html,text):
+def adolf(id,targetid,text):
   global fundysymbol
   change_text(targetid,text)
-  change_innerhtml(id,html)
+  change_innerhtml(id,fundysymbol)
 fundysymbol='<a href=\"https://www.youtube.com/watch?v=CEUPIET-r78\" target=\"_blank\" id=\"aadolf\">'
